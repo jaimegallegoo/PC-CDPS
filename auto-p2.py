17b0a90dc701300b09d4f3dae96240c80eb61588
jaimegallegoo <jaimegallegochillon@gmail.com>
2023-12-06 19:51:22 +0100

    Completar auto-p2

--- auto-p2.py
+++ auto-p2.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python
-
-from lib_mv import MV
-from lib_mv import Red
 import logging, sys
+from lib_mv import MV, Red
 
 def init_log():
     # Creacion y configuracion del logger
@@ -17,21 +14,62 @@ def init_log():
 def pause():
     programPause = input("Press the <ENTER> key to continue...")
 
-# Main
-init_log()
-print('CDPS - mensaje info1')
+def main():
+    if len(sys.argv) != 2:
+        print("Uso: python script.py <orden>")
+        return
 
-# Creación de las máquinas virtuales
-s1 = MV('s1')
-pause()
-s1.crear_mv('cdps-vm-base-pc1.qcow2', 'LAN2', False)
-pause()
+    orden = sys.argv[1]
 
-# Creación de la red
-red = Red('red')
-pause()
-red.crear_red()
-pause()
+    # Crear las clases MV
+    s1 = MV("s1")
+    s2 = MV("s2")
+    s3 = MV("s3")
+    c1 = MV("c1")
+    host = MV("host")
+    lb = MV("lb")
+    red = Red("red")
 
-# Arrancar las máquinas virtuales
-s1.arrancar_mv()
+    if orden == "crear":
+        # Realizar operaciones relacionadas con la creación de máquinas virtuales y redes
+        red.crear_red()
+        s1.crear_mv("cdps-vm-base-pc1.qcow2", "LAN2", False)
+        s2.crear_mv("cdps-vm-base-pc1.qcow2", "LAN2", False)
+        s3.crear_mv("cdps-vm-base-pc1.qcow2", "LAN2", False)
+        c1.crear_mv("cdps-vm-base-pc1.qcow2", "LAN1", False)
+        host.crear_mv("cdps-vm-base-pc1.qcow2", "LAN1", False)
+        lb.crear_mv("cdps-vm-base-pc1.qcow2", "null", True)
+
+    elif orden == "arrancar":
+        # Realizar operaciones relacionadas con el arranque de máquinas virtuales
+        s1.arrancar_mv()
+        s2.arrancar_mv()
+        s3.arrancar_mv()
+        c1.arrancar_mv()
+        host.arrancar_mv()
+        lb.arrancar_mv()
+
+    elif orden == "parar":
+        # Realizar operaciones relacionadas con el paro de máquinas virtuales
+        s1.parar_mv()
+        s2.parar_mv()
+        s3.parar_mv()
+        c1.parar_mv()
+        host.parar_mv()
+        lb.parar_mv()
+
+    elif orden == "liberar":
+        # Realizar operaciones relacionadas con la liberación de recursos
+        s1.liberar_mv()
+        s2.liberar_mv()
+        s3.liberar_mv()
+        c1.liberar_mv()
+        host.liberar_mv()
+        lb.liberar_mv()
+        red.liberar_red()
+
+    else:
+        print(f"Orden no reconocida: {orden}")
+
+if __name__ == "__main__":
+    main()
